983233aa7281d70b76d340cfe2c87f6edfd46c45
Ondrej Babec <ond.babec@gmail.com>
2020-02-11 09:57:12 +0100

    Cat

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -3,14 +3,14 @@ set -e
 
 sudo apt install docker.io
 sudo systemctl unmask docker
-sudo systemctl start docker
-
-sudo systemctl stop docker
 
 sudo mkdir /mnt/docker
 #sudo echo 'DOCKER_OPTS="-dns 8.8.8.8 -dns 8.8.4.4 -g /mnt/docker"' > /etc/default/docker
 
+sudo cat /lib/systemd/system/docker.service
 sudo sh -c "sed -i 's#ExecStart=/usr/bin/docker daemon -H fd://#ExecStart=/usr/bin/docker daemon -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
+sudo cat /lib/systemd/system/docker.service
+
 
 sudo systemctl daemon-reload
 sudo rsync -aqxP /var/lib/docker/ /mnt/docker
