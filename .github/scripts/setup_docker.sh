caf4410a5c77251055152eca4eaf9877c06af575
Ondrej Babec <ond.babec@gmail.com>
2020-02-11 09:45:43 +0100

    sed line

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install docker.io
+sudo systemctl unmask docker
+sudo systemctl start docker
 
 sudo systemctl stop docker
 
 sudo mkdir /mnt/docker
 #sudo echo 'DOCKER_OPTS="-dns 8.8.8.8 -dns 8.8.4.4 -g /mnt/docker"' > /etc/default/docker
 
-sudo sh -c "echo 'ExecStart=/usr/bin/docker daemon -g /new/path/docker -H fd://' >> /lib/systemd/system/docker.service"
+sudo sh -c "sed -i 's#ExecStart=/usr/bin/docker daemon -H fd://#ExecStart=/usr/bin/docker daemon -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
 
 sudo systemctl daemon-reload
 sudo rsync -aqxP /var/lib/docker/ /mnt/docker
