349d1d14660ab4e5cc194323c978a7a0b7599b55
Ondrej Babec <ond.babec@gmail.com>
2020-02-11 07:20:41 +0100

    Change image storage

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo apt install docker.io
 sudo systemctl unmask docker
+
+sudo systemctl start docker
+sudo systemctl stop docker
+
+mkdir /mnt/docker
+ln -s /mnt/docker /var/lib/docker
+
 sudo systemctl start docker
