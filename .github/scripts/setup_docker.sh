26f95f166488da673c51b686a56767256fa200b5
Ondrej Babec <ond.babec@gmail.com>
2020-02-11 07:24:03 +0100

    Grep error

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-sudo apt install docker.io
-sudo systemctl unmask docker
+set -e
 
-sudo systemctl start docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 sudo systemctl stop docker
 
 sudo mkdir /mnt/docker
+echo 'DOCKER_OPTS="-dns 8.8.8.8 -dns 8.8.4.4 -g /mnt/docker"' > /etc/default/docker
+
+echo "Creating symlink"
+
 sudo ln -s /mnt/docker /var/lib/docker
 
 sudo systemctl start docker
